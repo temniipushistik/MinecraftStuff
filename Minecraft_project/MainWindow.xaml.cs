82de11977cfb8839e48023310dcab781604337e3
Gennadii Koshelev <temniipushistik@gmail.com>
2020-09-20 20:56:18 +0300

    the welcoming windows

---
+++ Minecraft_project/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Minecraft_project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        public String Login { get; set; }
+
+
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void mainButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (textLogin.Text!= "")
+            {
+                Login = textLogin.Text;
+                welcome.Content = "Your name is "+Login;
+            }
+            else
+             welcome.Content = "The required field \r\n is empty";
+        }
+    }
+}
